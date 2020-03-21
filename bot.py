39569416d6ff24b8ea66ca6e873d6848be57fc20
Hamiax <43396618+Hamiax@users.noreply.github.com>
2020-03-20 21:59:51 -0500

    sends message to new members

--- bot.py
+++ bot.py
@@ -13,4 +13,8 @@ client = discord.Client()
 async def on_ready():
     print(str(client.user) + ' has connected to Discord!')
 
+@client.event
+async def on_member_join(member):
+    member.send("All members of Rochester Student/Parent Help server must use their real first and last name as their nickname in order to post messages. You can send a message to any moderator to change your nickname.")
+
 client.run(TOKEN)
